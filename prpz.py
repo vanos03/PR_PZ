6d79035f4fbadf24ad0c3ca64f89c0a03f5d4152
Your Name <you@example.com>
2024-04-21 09:13:59 -0400

    123

--- prpz.py
+++ prpz.py
@@ -1,8 +1,31 @@
 import re
 import requests
-# import argparse
+import argparse
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+
+def get_args():
+    args = argparse.ArgumentParser(description='Parser by domain company')
+
+    args.add_argument('-u', '--url',  
+                      required=True, 
+                      help='Target domain address')
+    args.add_argument('-o', '--out', 
+                      help='Set output file')
+    args.add_argument('-p', '--links_protocol', 
+                      default=['https'],
+                      nargs='+', 
+                      help='Set type parse links (default = https)')
+    args.add_argument('-s', '--show_err',
+                      action='store_true', 
+                      help='Show bad links')
+    args.add_argument('-v', '--visible_links',
+                      action='store_true', 
+                      help='Show bad links')
+
+    args = args.parse_args()
+
+    return args
 
 def fetch_page(url):
     try:
@@ -10,48 +33,61 @@ def fetch_page(url):
         if response.status_code == 200:
             return response.text
         else:
-            print("Ошибка при получении страницы:", response.status_code)
+            if args.show_err == True:
+                print(response.status_code, "Ошибка при получении страницы: ", url)
             return None
     except Exception as e:
-        print("Ошибка:", e)
+        if args.show_err == True:
+            print("Ошибка: ", e, "url: ", url)
         return None
 
-def extract_links(html, base_url):
+def extract_links(html, base_url, f_out, visited_urls):
     soup = BeautifulSoup(html, 'html.parser')
     links = soup.find_all('a', href=True)
     extracted_links = []
+   
     for link in links:
-        href = link['href']
-        if href.startswith('/'):
-            extracted_links.append(urljoin(base_url, href))
-        elif re.match(r'^https?://', href):
-            extracted_links.append(href)
-        elif re.match(r'^http?://', href):
-            extracted_links.append(href)
-        elif re.match(r'^ftp://', href):
-            extracted_links.append(href)
+        for prtcl in args.links_protocol:
+            href = link['href']
+            if href.startswith('/'):
+                extracted_links.append(urljoin(base_url, href))
+            elif re.match(f'{prtcl}://', href):
+                extracted_links.append(href)
+                # if href not in visited_urls:
+                #     if args.visible_links:
+                #         print(href)
+                #     if f_out is not None:
+                #         f_out.write(href + '\n')
+                #     visited_urls.add(href)
 
     return extracted_links
 
-
-def recursive_link_parser(start_url, target_domain, visited_urls=set()):
-    if start_url in visited_urls:
-        return
+def recursive_link_parser(start_url, target_domain, f_out, visited_urls):
     visited_urls.add(start_url)
-    print(start_url)
     html = fetch_page(start_url)
     if html:
-        links = extract_links(html, start_url)
+        links = extract_links(html, start_url, f_out, visited_urls)
+        
         for link in links:
+
+            if args.visible_links:
+                print(link)
+            if f_out is not None:
+                f_out.write(link + '\n')
             parsed_link = urlparse(link)
             if parsed_link.netloc == target_domain:
-                recursive_link_parser(link, target_domain, visited_urls)
+                if link not in visited_urls:
+                    recursive_link_parser(link, target_domain, f_out, visited_urls)
 
+def start_parse(args):
+    start_url = args.url
+    f_out = None
+    if args.out is not None:
+        f_out = open(args.out, 'w+')
+    target_domain = urlparse(start_url).netloc
+    visited_urls = set()
+    recursive_link_parser(start_url, target_domain, f_out, visited_urls)
 
 if __name__ == "__main__":
-
-    # parser = argparse.ArgumentParser(description='Great Description To Be Here')
-
-    start_url = "https://www.mirea.ru"
-    target_domain = urlparse(start_url).netloc
-    recursive_link_parser(start_url, target_domain)
+    args = get_args()
+    start_parse(args)
